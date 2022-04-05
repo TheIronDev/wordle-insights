81cd05208104ec9a865cc83c279e7125de758ea2
Tyler Stark <theirondeveloper@gmail.com>
2022-04-04 23:21:03 -0500

    Letting eslint do its eslinting thing
    `firebase deploy` wanted to lint everything, but the status was not quo.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
-import {useState} from 'react'
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import GameComponent from '../components/Game'
-import styles from '../styles/Home.module.css'
-import {signInWithPopup, signOut, onAuthStateChanged}  from "firebase/auth";
-import {auth, googleAuthProvider} from '../firebase'
+import React from 'react';
+import {useState} from 'react';
+import type {NextPage} from 'next';
+import Head from 'next/head';
+import GameComponent from '../components/Game';
+import styles from '../styles/Home.module.css';
+import {signInWithPopup, signOut, onAuthStateChanged} from 'firebase/auth';
+import {auth, googleAuthProvider} from '../firebase';
 
 
 type User = {
@@ -14,42 +15,57 @@ type User = {
 }
 
 function getUserAuthNavBar(currentUser:User) {
-    if (currentUser.isLoggedIn) {
-        return (
-            <div>
-                {currentUser.displayName}
-                <button onClick={() => signOut(auth)}>Login Out</button>
-            </div>
-        )
-    }
+  if (currentUser.isLoggedIn) {
     return (
+      <div>
+        {currentUser.displayName}
+        <button onClick={() => signOut(auth)}>Login Out</button>
+      </div>
+    );
+  }
+  return (
 
     <div>
-        <button onClick={() => signInWithPopup(auth, googleAuthProvider)}>Login with Google </button>
+      <button onClick={() => signInWithPopup(auth, googleAuthProvider)}>
+        Login with Google
+      </button>
     </div>
-    )
+  );
 }
 
 const Home: NextPage = (props) => {
   const [currentUser, setCurrentUser] = useState({
-      displayName: '',
-      isLoggedIn: false,
+    displayName: '',
+    isLoggedIn: false,
   } as User);
-  onAuthStateChanged(auth,(user) => {
-   setCurrentUser({displayName: user?.displayName || '', isLoggedIn: !!user, uid: user?.uid} as User);
+  onAuthStateChanged(auth, (user) => {
+    setCurrentUser({
+      displayName: user?.displayName || '',
+      isLoggedIn: !!user,
+      uid: user?.uid,
+    } as User);
   });
 
   const userAuthNavBarSection = getUserAuthNavBar(currentUser);
-  const mainContent = currentUser && currentUser.uid ? <GameComponent uid={currentUser.uid} /> : <div>Login required</div>;
+  const mainContent = currentUser && currentUser.uid ?
+    <GameComponent uid={currentUser.uid} /> :
+    <div>Login required</div>;
+  const viewportMetadata = [
+    'width=device-width',
+    'initial-scale=1.0',
+    'maximum-scale=1.0',
+    'user-scalable=0',
+  ].join(', ');
 
   return (
     <div className={styles.container}>
       <Head>
         <title>World Insights</title>
-        <meta name="description" content="Wordle Insights - Wordle with an emphasis on data" />
+        <meta
+          name="description"
+          content="Wordle Insights - Wordle with an emphasis on data" />
         <link rel="icon" href="/favicon.ico" />
-          <meta name="viewport"
-           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+        <meta name="viewport" content="{viewportMetadata}" />
       </Head>
 
       <nav>
@@ -57,15 +73,15 @@ const Home: NextPage = (props) => {
           Worldle Insights
         </h1>
         <div>
-            {userAuthNavBarSection}
+          {userAuthNavBarSection}
         </div>
       </nav>
 
       <main className={styles.main}>
-          {mainContent}
+        {mainContent}
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
